44848b3fbe62f5a09b860e0f4acd74019778973f
Frankenstein0708 <92561484+Frankenstein0708@users.noreply.github.com>
2023-10-31 14:09:06 +0530

    Time: 2 ms (97.97%), Space: 40.9 MB (73.3%) - LeetHub

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+
+        Arrays.sort(cost);
+        int sum = 0;
+        if(cost.length ==1) return cost[0];
+
+        for(int i = cost.length -1;i>0;i--)
+        {
+            sum+=cost[i] + cost[--i];
+            if (i - 2 == 0) {
+                sum += cost[0];
+                return sum;
+            }            
+            --i;
+        }
+        return sum;        
+        
+    }
+}
