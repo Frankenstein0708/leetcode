587bb91fc160101fba8d62fe53878aef6ed8b504
Frankenstein0708 <92561484+Frankenstein0708@users.noreply.github.com>
2023-10-31 20:32:54 +0530

    Time: 2 ms (98.31%), Space: 43.8 MB (99.15%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int low =0;
+        int n= s.length();
+        int high= s.length();
+        int[] res = new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='I')
+            {
+                res[i]=low;
+                low++;
+            }else
+            {
+                res[i]=high;
+                high--;
+            }
+        }
+        res[s.length()]=high;
+        return res;
+
+    }
+}
